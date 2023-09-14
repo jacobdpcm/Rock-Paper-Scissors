387ea6c748c87f63eaf6beec5859f90dbc43b41b
JacobPCM <jacobd@postcardmania.com>
2023-09-14 17:54:32 -0400

    Configure style Set up of display of computer's choice

--- index.js
+++ index.js
@@ -12,11 +12,30 @@ function getComputerChoice() {
     return x;
 }
 
-//Set up every possible outcome for a round
+//Set up computer choise
 function playRound(playerScore, computerScore) {
     const computerSelection = getComputerChoice();
     const round = document.querySelector('.round')
 
+//Set up Displaying Computer's choice
+    const comChoice = document.createElement('img')
+    comChoice.classList.add('comimage') 
+    comChoice.src = "./images/computer.jpg"
+    const comrock = document.querySelector('.comrock')
+    const compaper = document.querySelector('.compaper')
+    const comscissor = document.querySelector('.comscissor')
+    comrock.innerHTML = ''
+    compaper.innerHTML = ''
+    comscissor.innerHTML = ''
+    if(computerSelection === 'rock'){
+        comrock.appendChild(comChoice);
+    } else if(computerSelection === 'paper'){
+        compaper.appendChild(comChoice)
+    } else if(computerSelection === 'scissor'){
+        comscissor.appendChild(comChoice)
+    }
+
+//Set up every possible outcome for a round
     if (playerSelection === computerSelection) {
         round.textContent = "It's a tie";
         console.log("It's a tie")
@@ -81,8 +100,10 @@ const btnPaper = document.querySelector('.paper')
 const btnScissor = document.querySelector('.scissor')
 const score = document.querySelector('.game')
 
+
 let x = null
 
+//User choice inputs
 btnRock.addEventListener('click', function(){
     playerSelection = 'rock';
     x = playRound(playerScore, computerScore);
